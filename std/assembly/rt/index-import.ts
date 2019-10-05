b72eefb85e4e776bbf56023cf46ad7d53a12b9e4
techforindustry <sam@tech4ind.com>
2019-10-05 12:14:16 -0600

    Add Imports rt

--- std/assembly/rt/index-import.ts
+++ std/assembly/rt/index-import.ts
@@ -1,4 +1,4 @@
-// import "rt/index-full";
+// declare imports that will be required and put them into the asr namespace
 @global @external("env","__alloc")
 export declare function __alloc(size: usize, id: u32): usize;
 @global @external("env","__realloc")
@@ -11,19 +11,5 @@ export declare function __retain(ref: usize): usize;
 export declare function __release(ref: usize): void;
 @global @external("env","__collect")
 export declare function __collect(): void;
-// @global @external("env","__rtti_base ")
-// export declare const __rtti_base:usize;
-// @global @external("env","__typeinfo ")
-// export declare function __typeinfo(id: u32): u32;
-// @global @external("env","__instanceof ")
-// export declare function __instanceof(ref: usize, superId: u32): bool;
 @global @external("env","__visit")
 export declare function __visit(ref: usize, cookie: i32): void;
-// @global @external("env","__visit_globals ")
-// export declare function __visit_globals(cookie: u32): void;
-// @global @external("env","__visit_members ")
-// export declare function __visit_members(ref: usize, cookie: u32): void;
-// @global @external("env","__allocArray ")
-// export declare function __allocArray(length: i32, alignLog2: usize, id: u32, data?: usize): usize;
-// @global @external("env","ASC_RTRACE ")
-// export declare const ASC_RTRACE: boolean;
